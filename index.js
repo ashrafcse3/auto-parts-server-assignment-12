4f50344e4e3abfd947204f7bc4e714717f2705ff
Ashraful Alam <ashrafcse3@gmail.com>
2022-11-24 23:57:25 +0000

    category delete, update, cat by id is done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 6000;
@@ -23,9 +23,48 @@ async function run() {
         const productsCollection = client.db('resaleProducts').collection('products');
         const categoriesCollection = client.db('resaleProducts').collection('categories');
 
-        // Products api's 
+        // categories api's 
         app.post('/categories', async (req, res) => {
             const result = await categoriesCollection.insertOne(req.body);
+            res.send(result);
+        })
+
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const categories = await categoriesCollection.find(query).toArray();
+            res.send(categories);
+        })
+
+        app.get('/categories/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log(id);
+            const query = { _id: ObjectId(id) };
+            const category = await categoriesCollection.findOne(query);
+            res.send(category);
+        })
+
+        app.patch('/categories', async (req, res) => {
+            const data = req.body;
+
+            const filter = { _id: ObjectId(data._id) };
+            const options = { upsert: true };
+            const updateCategory = {
+                $set: {
+                    name: data.name,
+                    image: data.image
+                }
+            };
+
+            const result = await categoriesCollection.updateOne(filter, updateCategory, options);
+
+            res.send(result);
+        })
+
+        app.delete('/categories/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await categoriesCollection.deleteOne(query);
+
             res.send(result);
         })
 
